3055d8ba14b5effebc31ef667787b4528936f13e
Rohan Kumar <rohankumarmenu@gmail.com>
2025-06-26 02:48:20 +0530

    Subset_Sum2

--- Recursion/SusetSum2/Subsetsum2.cpp
+++ Recursion/SusetSum2/Subsetsum2.cpp
@@ -1,1 +1,23 @@
-// Today
+class Solution {
+    public:
+        void recurse(int index, vector<vector<int>>&ans, vector<int>&store, vector<int>&nums) {
+            int n = nums.size();
+             ans.push_back(store);
+            for (int i = index; i < n; i++) {
+               
+                if(i!=index && nums[i]==nums[i-1]){
+                    continue;
+                }
+                store.push_back(nums[i]);
+                recurse(i + 1, ans, store, nums);
+                store.pop_back();
+            }
+        }
+        vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+         sort(nums.begin(),nums.end());
+            vector<vector<int>> ans;
+            vector<int> store;
+            recurse(0, ans, store, nums);
+            return ans;
+        }
+    };
