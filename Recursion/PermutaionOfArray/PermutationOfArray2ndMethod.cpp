af16c72c72efc1dcfc6905c12d80663736afbecf
Rohan Kumar <rohankumarmenu@gmail.com>
2025-07-01 02:20:37 +0530

    PermutationOfArray2ndMethod

---
+++ Recursion/PermutaionOfArray/PermutationOfArray2ndMethod.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+vector<vector<int>>ans;
+
+void recurse(vector<int>& nums,int ind){
+   if(nums.size()==ind){
+    ans.push_back(nums);
+    return;
+   }
+   
+     
+   for(int i=ind;i<nums.size();i++){
+    swap(nums[ind],nums[i]);
+    recurse(nums,ind+1);
+    swap(nums[ind],nums[i]);
+   }
+
+}
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>store;
+        recurse(nums,0);
+            return ans;
+    }
+};
