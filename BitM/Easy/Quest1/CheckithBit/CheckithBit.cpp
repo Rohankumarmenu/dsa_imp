3a3d03b2f7385a0dba6be746abb6adc6d693d22f
Rohan Kumar <rohankumarmenu@gmail.com>
2025-08-07 01:58:51 +0530

    CheckIthBit

---
+++ BitM/Easy/Quest1/CheckithBit/CheckithBit.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        
+        if((n>>k) & 1 != 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
