227705d8f1f998434f309bae2c3ba9be34454cf6
Rohan Kumar <rohankumarmenu@gmail.com>
2026-06-20 12:33:15 +0530

    KClosetElem

---
+++ Pattern/Heap/KClosetType2/FindKClosetElem/FindKClosetElem.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    vector<int> findClosestElements(vector<int> &arr, int k, int x)
+    {
+        priority_queue<pair<int, int>> pq;
+        vector<pair<int, int>> findPair;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            int dist = abs(arr[i] - x);
+
+            pq.push({dist, i});
+            if (pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        while (!pq.empty())
+        {
+            ans.push_back(arr[pq.top().second]);
+            pq.pop();
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
