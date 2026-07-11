3affddae4fd4e4026cc165a7561a715033ece9d3
Rohan Kumar <rohankumarmenu@gmail.com>
2026-07-11 12:20:25 +0530

    FindPivotIndex

---
+++ Pattern/PrefixSum/FindPivotIndex/FindPivotIndex.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        int curr=0;
+        for(auto i:nums){
+           sum+=i;
+        }
+
+        for(int i=0;i<nums.size();i++){
+           
+          if(curr==sum-curr-nums[i]){
+            return i;
+          }
+           curr+=nums[i];
+        }
+        return -1;
+    }
+};
