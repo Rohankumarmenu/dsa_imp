9598c11bedceeb1bcca584d31ae20e6ec047b59b
Rohan Kumar <rohankumarmenu@gmail.com>
2026-08-08 12:54:05 +0530

    Same TRee

---
+++ Pattern/Tree/Mirror&Symmetry/Invert_BT/Invert.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        if(root==NULL){  return NULL ;      }
+        queue<TreeNode*>q;
+        
+        q.push(root);
+          while(!q.empty()){
+            
+             int n=q.size();
+             for(int i=0;i<n;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                TreeNode* k=node->left;
+               node->left=node->right;
+               node->right=k;
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+               
+
+            
+             }
+             
+                 
+          }
+          return root;
+    }
+};
